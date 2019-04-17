b90aaca974cd83a256aa3d1513f110947b16e8d1
yuridiablo <yuridiablo@gmail.com>
2019-04-17 19:55:08 +0800

    vendor start

---
+++ CA107G3/src/tools/ShowImg.java
@@ -0,0 +1,88 @@
+package tools;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 資料庫顯示圖片
+ */
+@WebServlet("/ShowImage")
+
+public class ShowImg extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Connection con = null;
+	Statement stmt = null;
+	ResultSet rs = null;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("doGet");
+		doPost(req, res);
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		res.setContentType("image/gif");
+		ServletOutputStream out = res.getOutputStream();
+
+		try {
+			Statement stmt = con.createStatement();
+			String SQL ="SELECT MENU_PIC FROM RESTAURANT_MENU WHERE MENU_NO = " + req.getParameter("menu_no");
+//			String SQL ="SELECT MENU_PIC FROM RESTAURANT_MENU WHERE MENU_NO = 'RM00000008'";
+			System.out.println(SQL);
+			ResultSet rs = stmt.executeQuery(SQL);
+			
+			
+			
+			if (rs.next()) {
+			
+				BufferedInputStream in = new BufferedInputStream(rs.getBinaryStream("MENU_PIC"));
+				byte[] buf = new byte[4 * 1024]; // 4K buffer
+				int len;
+				while ((len = in.read(buf)) != -1) {
+					out.write(buf, 0, len);
+				}
+				
+				in.close();
+			} else {
+				res.sendError(HttpServletResponse.SC_NOT_FOUND);
+			}
+			rs.close();
+			stmt.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public void init() throws ServletException {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "CA107G3", "123456");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void destroy() {
+		try {
+			if (con != null)
+				con.close();
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+
+	}
+}
